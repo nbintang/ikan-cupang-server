02bf583921475ca711650a99a55705e59b77bb50
nbintang <nbintangh@gmail.com>
2025-02-11 11:07:51 +0700

    fix: deploy

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,4 @@
 import { Context, Hono } from "hono";
-import { html } from "hono/html";
 import authRoutes from "./auth";
 import protectedRoutes from "./protected";
 import { refreshToken } from "@/controller/auth";
@@ -15,20 +14,5 @@ export const introductions = (c: Context) => {
     message: "See Our API through Here!",
   };
   const url = "/api";
-  return c.html(
-    html`<html>
-      <head>
-        <title>Hello Hono!</title>
-      </head>
-      <body
-        style="display: flex; align-items: center; justify-content: center; height: 100vh;"
-      >
-        <a
-          href="${url}"
-          style="color: white; background-color: black; text-decoration: none; padding: 10px; border-radius: 5px; "
-          >${payload.message}</a
-        >
-      </body>
-    </html>`
-  );
+  return c.json(payload, 200);
 };
